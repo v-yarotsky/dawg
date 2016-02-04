99c7ce9f6b541a5d6ded05887ad24b45171731c3
Vlad Yarotsky <vlad@remind101.com>
2016-02-04 09:43:06 -0800

    Nicer errors

--- cmd/dawg/dawg.go
+++ cmd/dawg/dawg.go
@@ -19,49 +19,57 @@ func main() {
 
 	switch true {
 	case *makeWorkflow:
-		makeAlfredWorkflow(c)
+		handleError(makeAlfredWorkflow(c))
 	case *service != "":
 		pattern := flag.Arg(0)
-		printAlfredXML(c, *service, pattern)
+		handleError(printAlfredXML(c, *service, pattern))
 	default:
 		flag.Usage()
 		os.Exit(1)
 	}
 }
 
-func printAlfredXML(c dawg.Config, service, pattern string) {
+func printAlfredXML(c dawg.Config, service, pattern string) error {
 	alfredOut, err := dawg.Filter(c, service, pattern)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("could not filter through config: %v", err)
 	}
 
 	rawXML, err := alfredOut.MakeXML()
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("could not prepare xml output for Alfred: %v", err)
 	}
 	fmt.Println(string(rawXML))
+	return nil
 }
 
-func makeAlfredWorkflow(c dawg.Config) {
+func makeAlfredWorkflow(c dawg.Config) error {
 	plist := dawg.MakeWorkflowPList(c)
 	out, _ := plist.PListWithHeader()
 
 	zipfile, err := dawg.MakeWorkflowZIP(out)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("could not make a workflow zip file: %v", err)
 	}
 
 	f, err := os.Create("DAWG.alfredworkflow")
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("could not open workflow zip file for writing: %v", err)
 	}
 
 	_, err = io.Copy(f, zipfile)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("could not write to the workflow zip file: %v", err)
 	}
 
 	err = f.Close()
+	if err != nil {
+		return fmt.Errorf("could not close the workflow zip file: %v", err)
+	}
+	return nil
+}
+
+func handleError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
